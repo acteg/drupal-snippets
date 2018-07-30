563c853780d8e7059a6dca0dad7d6ac6ad296ef2
acteg <ateguia@actency.fr>
2018-07-30 12:36:34 +0200

    Update FormApi.php

--- FormApi/FormApi.php
+++ FormApi/FormApi.php
@@ -6,19 +6,19 @@ $validators = [
       'file_validate_size' => [5242880], // 5 mo
     ];
 
-    $form['file'] = [
-      '#type' => 'file',
-      '#title' => $this->t('Attachment', [], ['context' => 'Contact form']),
-      '#description' => [
-        '#theme' => 'file_upload_help',
-        '#description' => $this->t('A Gettext Portable Object file.'),
-        '#upload_validators' => $validators,
-      ],
-      '#size' => 50,
-      '#upload_validators' => $validators,
-      '#attributes' => ['class' => ['file-import-input']],
-    ];
+$form['file'] = [
+'#type' => 'file',
+'#title' => $this->t('Attachment', [], ['context' => 'Contact form']),
+'#description' => [
+  '#theme' => 'file_upload_help',
+  '#description' => $this->t('A Gettext Portable Object file.'),
+  '#upload_validators' => $validators,
+],
+'#size' => 50,
+'#upload_validators' => $validators,
+'#attributes' => ['class' => ['file-import-input']],
+];
 
-    $form['keys']['#attributes']['placeholder'] = 'Search some things';
-    $form['actions']['submit']['#validate'][] = array($this, 'validateContactForm');
-    $form['actions']['submit']['#submit'][] = array($this, 'submitContactForm');
+$form['keys']['#attributes']['placeholder'] = 'Search some things';
+$form['actions']['submit']['#validate'][] = array($this, 'validateContactForm');
+$form['actions']['submit']['#submit'][] = array($this, 'submitContactForm');
